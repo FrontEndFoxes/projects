eee0a8330e5ed29c655d2908394b5db6115ab1e5
NataliaTepluhina <tarya.se@gmail.com>
2018-05-19 12:02:11 +0300

    * added web mini-workshop folder

---
+++ mini-2-web/src/components/Dog.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card class="dog-card">
+    <v-card-media
+      height="150px"
+      :src="dog"></v-card-media>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="$emit('remove')">
+        <v-icon>delete</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    dog: {
+      type: String,
+    },
+  },
+};
+</script>
