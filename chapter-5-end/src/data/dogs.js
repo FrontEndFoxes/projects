7c223644f71e4a35d4ae1e192e9d63089087f2f7
Michele Cynowicz <michele.cynowicz@voxmedia.com>
2019-08-05 20:46:39 -0400

    update the same file in subsequent chapters

--- chapter-5-end/src/data/dogs.js
+++ chapter-5-end/src/data/dogs.js
@@ -1,42 +1,42 @@
-export default [
-  {
-    name: 'Max',
-    breed: 'husky',
-    img: 'https://dog.ceo/api/img/husky/n02110185_1469.jpg',
-  },
-  {
-    name: 'Rusty',
-    breed: 'shiba',
-    img: 'https://dog.ceo/api/img/shiba/shiba-13.jpg',
-  },
-  {
-    name: 'Rocco',
-    breed: 'boxer',
-    img: 'https://dog.ceo/api/img/boxer/n02108089_14112.jpg',
-  },
-  {
-    name: 'Zoey',
-    breed: 'beagle',
-    img: 'https://dog.ceo/api/img/beagle/n02088364_11136.jpg',
-  },
-  {
-    name: 'Duke',
-    breed: 'doberman',
-    img: 'https://dog.ceo/api/img/doberman/n02107142_4653.jpg',
-  },
-  {
-    name: 'Lily',
-    breed: 'malamute',
-    img: 'https://dog.ceo/api/img/malamute/n02110063_1104.jpg',
-  },
-  {
-    name: 'Winston',
-    breed: 'pug',
-    img: 'https://dog.ceo/api/img/pug/n02110958_15626.jpg',
-  },
-  {
-    name: 'Angel',
-    breed: 'samoyed',
-    img: 'https://dog.ceo/api/img/samoyed/n02111889_4470.jpg',
-  },
+export const Dogs = [
+	{
+		name: 'Max',
+		breed: 'husky',
+		img: 'https://images.dog.ceo/breeds/husky/n02110185_1469.jpg',
+	},
+	{
+		name: 'Rusty',
+		breed: 'shiba',
+		img: 'https://images.dog.ceo/breeds/shiba/shiba-13.jpg',
+	},
+	{
+		name: 'Rocco',
+		breed: 'boxer',
+		img: 'https://images.dog.ceo/breeds/boxer/n02108089_14112.jpg',
+	},
+	{
+		name: 'Zoey',
+		breed: 'beagle',
+		img: 'https://images.dog.ceo/breeds/beagle/n02088364_11136.jpg',
+	},
+	{
+		name: 'Duke',
+		breed: 'doberman',
+		img: 'https://images.dog.ceo/breeds/doberman/n02107142_4653.jpg',
+	},
+	{
+		name: 'Lily',
+		breed: 'malamute',
+		img: 'https://images.dog.ceo/breeds/malamute/n02110063_1104.jpg',
+	},
+	{
+		name: 'Winston',
+		breed: 'pug',
+		img: 'https://images.dog.ceo/breeds/pug/n02110958_15626.jpg',
+	},
+	{
+		name: 'Angel',
+		breed: 'samoyed',
+		img: 'https://images.dog.ceo/breeds/samoyed/n02111889_4470.jpg',
+	},
 ];
